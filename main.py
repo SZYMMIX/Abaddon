ff6732d3887974db645d4db9017862ae6040b743
Szymon Cimochowski <szycim10@gmail.com>
2026-01-02 02:47:51 +0100

    Refactor player movement and drawing logic

--- main.py
+++ main.py
@@ -2,24 +2,27 @@ import pygame
 import sys
 from settings import *
 from map import *
+from player import *
 
 class Game:
     def __init__(self):
         pygame.init()
 
-        pygame.mouse.set_visible(False) 
-        pygame.event.set_grab(True)
-        
         self.display = pygame.display.set_mode(RES)
         self.clock = pygame.time.Clock()
         self.running = True
         self.new_game()
 
+        pygame.mouse.set_visible(False) 
+        pygame.event.set_grab(True)
+
 
     def new_game(self):
         self.map = Map(self)
+        self.player = Player(self)
 
     def update(self):
+        self.player.update()
         pygame.display.flip()
         self.clock.tick(FPS)
         pygame.display.set_caption(f'{self.clock.get_fps() :.1f}')
@@ -27,6 +30,7 @@ class Game:
     def draw(self):
         self.display.fill('black')
         self.map.draw()
+        self.player.draw()
 
     def check_events(self):
         for event in pygame.event.get():
--- player.py
+++ player.py
@@ -33,4 +33,21 @@ class Player:
             dx += -speed * cos_a
             dy += -speed * sin_a
         if keys[pygame.K_a]:
-            dy += -speed * sin_a
+            dx += speed * sin_a
+            dy += -cos_a * speed
+        if keys[pygame.K_d]:
+            dx += -speed * sin_a
+            dy += cos_a * speed
+        
+        self.x += dx
+        self.y += dy
+        self.angle %= math.tau
+
+    def draw(self):
+        pygame.draw.circle(self.game.display, 'green', (self.x * 100, self.y * 100), 15)
+        pygame.draw.line(self.game.display, 'yellow', (self.x * 100, self.y * 100), 
+                         (self.x * 100 + WIDTH * math.cos(self.angle),
+                           self.y * 100 + HEIGHT * math.sin(self.angle)), 2)
+
+    def update(self):
+        self.movement()
