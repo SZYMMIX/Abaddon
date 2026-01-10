13bac4cbba541b6a6546fde4776c5d35c9ff3c3d
Szymon Cimochowski <szycim10@gmail.com>
2026-01-10 03:09:04 +0100

    Introduce player FOV and adjust the number of rays

--- raycasting.py
+++ raycasting.py
@@ -11,23 +11,27 @@ class RayCasting:
 
     def ray_cast(self):
         ox, oy = self.game.player.x, self.game.player.y
-        x, y = ox, oy 
-        ray_angle = self.game.player.angle
+        ray_angle = self.game.player.angle - HALF_FOV
         
-        sin_a = math.sin(ray_angle)
-        cos_a = math.cos(ray_angle)
 
-        for depth in range(MAX_DEPTH): 
-            x += 0.01 * cos_a
-            y += 0.01 * sin_a
+        for _ in range(NUM_RAYS):
 
-            if (int(x), int(y)) in self.game.map.world_map:
-                pygame.draw.line(self.game.display, 'red', (ox * 100, oy * 100),
-                                (x * 100, y * 100), 2)
-                
-                pygame.draw.circle(self.game.display, 'green', (x * 100, y * 100), 5)
-                
-                return 
+            sin_a = math.sin(ray_angle)
+            cos_a = math.cos(ray_angle)
+            x, y = ox, oy 
+
+            for depth in range(int(MAX_DEPTH * 100)): 
+                x += 0.01 * cos_a
+                y += 0.01 * sin_a
+
+                if (int(x), int(y)) in self.game.map.world_map:
+                    pygame.draw.line(self.game.display, 'red', (ox * 100, oy * 100),
+                                    (x * 100, y * 100), 2)
+                    
+                    pygame.draw.circle(self.game.display, 'green', (x * 100, y * 100), 5)
+                    
+                    break 
+            ray_angle += DELTA_ANGLE
 
         pygame.draw.line(self.game.display, 'red', (ox * 100, oy * 100),
                         (x * 100, y * 100), 2)
